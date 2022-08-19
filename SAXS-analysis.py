db8353376d8082dc69ebae4142fae5b411049197
Paul Di Pasquale <43717388+pdipasquale@users.noreply.github.com>
2022-08-19 11:47:08 +1000

    Update SAXS-analysis.py
    I tried doing some log file stuff but i got stuck on line 24 trying to fill the I0 array with values. I'll test it some more later but for now here it is

--- SAXS-analysis.py
+++ SAXS-analysis.py
@@ -1,5 +1,6 @@
 # This code should run some real-time analysis of the data from any experiment we perform at the SAXS/WAXS beamline (or any beamline for that matter). #
-
+import numpy as np
+import matplotlib.pyplot as plt
 # The steps required for analysis are listed below. Each step is separated by a section, with an explanation on what needs to be done for analysis. #
 # Dat2Tif #
 # Convert raw image files into .tif for ease of import and analysis. #
@@ -7,6 +8,38 @@
 
 # Reformat Log Files #
 # Read in log files and reformat them to something actually readable, with headers and split the log file if necessary. place these reformatted files into a new file. #
+
+biglog = open("livelogfile.log", 'r')
+contents = biglog.readlines()
+with biglog as fp:
+    num_lines = sum(1 for line in fp)
+
+rows = list(range(1, num_lines))
+print(rows)
+I0 = []
+for row in rows:
+    row_n = contents[row]
+    split_lines = row_n.split(" ")
+    I0_str = split_lines[20]
+    I0[row] = float(I0_str.split('"')) # value is formatted as "number", need to remove that shit
+
+print(I0)
+
+#row_n = contents[row]
+#split_lines = row_1.split(" ")
+# Start at 5, 7 because the timestamp format is fucked
+
+# Below extracts the variable name and corresponding value
+# For the time being I0 is all we want, find where that occurs and plot that
+
+#lines = list(range(1,len(split_lines)))
+#for line in lines:
+#	print(line)
+#	print(split_lines[line])
+# I0 is at line 18/line 20
+#I0_str = split_lines[20]
+#I0 = I0_str.split('"') # value is formatted as "number", need to remove that shit
+#print(float(I0[1]))
 
 
 # DarkField correction. #
