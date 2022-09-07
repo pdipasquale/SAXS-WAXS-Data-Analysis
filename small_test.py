455906b91a5daccbbc7a4f4903957ee856081584
Paul Di Pasquale <paul.dipasquale.1994@gmail.com>
2022-09-07 22:38:46 +1000

    I GOT IT TO IMPORT THE IMAGE WOO

--- small_test.py
+++ small_test.py
@@ -5,22 +5,33 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from PIL import Image
 
-spool_directory = '/home/paul/Documents/SAXS-WAXS_test_data/raw/Nigel_Naked_Test2_Fine_run2/spool/SpoolDirectory'
-tif_directory = '/home/paul/Documents/SAXS-WAXS_test_data/fout'
+def read_tif(fname):
+    im = Image.open(fname)
+    print('IN: {} [Image size: {}, Image mode: {}]'.format(fname, im.size, im.mode))
+    return im
+
+#spool_directory = '/home/paul/Documents/SAXS-WAXS_test_data/raw/Nigel_Naked_Test2_Fine_run2/spool/SpoolDirectory'
+#tif_directory = '/home/paul/Documents/SAXS-WAXS_test_data/fout'
+tif_directory = './test_frame'
+frame2 = tif_directory + '/2.tif'
 
 # ---------------------------------------------- #
 # Need to import an image, the code for this is below:
 # Open image
-frame2 = tif_directory + '/2.tif'
+
 
 #img = mpimg.imread(frame2)
+#print(img)
 #imgplot = plt.imshow(img)
 #plt.show()
 
-img = Image.open(frame2)
-img.show() 
+#img = Image.open(frame2)
+#print(img)
+#img.show()
+test_frame = read_tif(frame2)
+image_array = np.array(test_frame)
+
+print(image_array)
+plt.imshow(image_array, cmap='gray')
+plt.show()
 # ---------------------------------------------- #
-
-#pyplot.imshow(im)
-
-#pyplot.show()
