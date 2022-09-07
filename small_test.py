4b820ba5558bbce83b1a2630436a4d2d337406a7
pdipasquale <paul.dipasquale.1994@gmail.com>
2022-09-07 21:12:40 +1000

    i can't get this working

--- small_test.py
+++ small_test.py
@@ -1,7 +1,8 @@
 # This code should run some real-time analysis of the data from any experiment we perform at the SAXS/WAXS beamline (or any beamline for that matter). #
+#from tkinter import Frame
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.image as img
+import matplotlib.image as mpimg
 from PIL import Image
 
 spool_directory = '/home/paul/Documents/SAXS-WAXS_test_data/raw/Nigel_Naked_Test2_Fine_run2/spool/SpoolDirectory'
@@ -10,22 +11,14 @@ tif_directory = '/home/paul/Documents/SAXS-WAXS_test_data/fout'
 # ---------------------------------------------- #
 # Need to import an image, the code for this is below:
 # Open image
-file = tif_directory + '/2.tif'
-im = Image.open(file)
-data = np.asarray(im)
-im.show()
-#im = img.imread(file)
-#data = np.array(im)
-#print(data.dtype)
-#print(data.shape)
-#print(im.format)
-#print(im.size)
-#print(im.mode)
-#print(im.dtype)
-#print(im.shape)
-#load_im.show()
-# Make into Numpy array
-#na = np.ndarray(im)
+frame2 = tif_directory + '/2.tif'
+
+#img = mpimg.imread(frame2)
+#imgplot = plt.imshow(img)
+#plt.show()
+
+img = Image.open(frame2)
+img.show() 
 # ---------------------------------------------- #
 
 #pyplot.imshow(im)
