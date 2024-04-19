6a5cad8a94b1d80900328c027b9d67d391c54168
J-K <jakejohnrogers@gmail.com>
2024-04-19 15:23:20 +1000

    Adding Transmission Function
    Adapted from previous MATLAB code, transmission() provides the complex transmission function based on energy, sample thickness, and wave parameters. Also included is code for interpolation and plotting results for on-the-fly verification.

---
+++ transmission.py
@@ -0,0 +1,50 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+# Written by: Jake J. Rogers
+# Date: 19/04/2024
+
+# ALL CONSTANTS ARE IN SI UNITS
+constants = {
+            'H'  : 6.62607015E-34,   # J
+            'eV' : 1.602176634E-19,  # J
+            'c'  : 299792458,        # m/s
+             }
+
+def transmission(energy, delta, beta, thickness):
+    """_summary_
+
+    Args:
+        beta (_float_): wave transmission parameter
+        delta (_float_): wave transmission parameter
+        thickness (_float_): sample thickness
+        energy (_float_): in units of keV
+
+    Returns:
+        _np.complex_: transmission function
+    """
+    
+    wavelength = constants['H'] * constants['c'] / (energy * constants['eV'])
+    
+    re_fac = np.exp(-((2*np.pi*beta)/wavelength)*thickness)
+    im_fac = np.exp(-1j*((2*np.pi*delta)/wavelength)*thickness)
+    
+    return re_fac * im_fac
+
+# TEST RUN
+thickness = 5E-6 # metres
+
+fname = '.\Fe.txt'
+data  = np.genfromtxt(fname, delimiter='  ', skip_header=2).transpose()
+energy, delta, beta = data
+
+energy_p = np.linspace(energy[0], energy[-1], 1000)
+delta_p  = np.interp(energy_p, energy, delta)
+beta_p   = np.interp(energy_p, energy, beta)
+
+trans = transmission(energy_p, delta_p, beta_p, thickness)
+
+
+plt.plot(energy_p, np.sqrt(np.real(trans)**2 + np.imag(trans)**2) / constants['eV'])
+plt.show()
