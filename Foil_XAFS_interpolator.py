87ad463d1d19d7f6ae7c88733cc450db21c4ac09
Paul Di Pasquale <paul.dipasquale.1994@gmail.com>
2024-04-21 13:33:51 +1000

    Finished the foil xafs interpolator

--- Foil_XAFS_interpolator.py
+++ Foil_XAFS_interpolator.py
@@ -2,8 +2,6 @@ import numpy as np
 import scipy
 import matplotlib.pyplot as plt
 
-# This script is for interpolating the energy ranges that we will be using for our scans, specifically to check the spacing and structure that the scan is covering.
-
 def convert_strings_to_floats(input_array):
     output_array = []
     for element in input_array:
@@ -12,25 +10,28 @@ def convert_strings_to_floats(input_array):
     return output_array
 
 # The filenames of the text files containing the reference data are:
-# Copper: "XAFS NIMS Database spectra\Cu-K_Cu-foil_Si311_50ms_140625.dat"
-# Nickel: "XAFS NIMS Database spectra\Ni-K_Ni-foil_Si311_10ms_131127.dat"
-# Iron: "XAFS NIMS Database spectra\Fe-K_Fe-foil_Si111_50ms_120621.dat"
 
-copper_ref = "XAFS NIMS Database spectra/Cu-K_Cu-foil_Si311_50ms_140625.txt"
+#copper_ref = "XAFS NIMS Database spectra/Cu-K_Cu-foil_Si311_50ms_140625.txt"
 nickel_ref = "XAFS NIMS Database spectra/Ni-K_Ni-foil_Si311_10ms_131127.txt"
 iron_ref = "XAFS NIMS Database spectra/Fe-K_Fe-foil_Si111_50ms_120621.txt"
 cobalt_ref = "XAFS NIMS Database spectra/Co-K_Co-foil_Si111_50ms_121117.txt"
+
+element_data = {
+    'Ni':(nickel_ref, 8333),
+    'Fe':(iron_ref, 7112),
+    'Co':(cobalt_ref, 7709)
+}
 # The structure of the reference files is shown below:
 # Data starts at row 15
 # columns are: Angle, angle, time, 2 (Absorption), 3 (Energy)
 # the last two columns are what we need for our interpolation
 
 # Choose your sample below:
-sample_dir = copper_ref
-edge = 8979
+element = 'Ni'
+sample_dir, edge = element_data[element]
 # Put the energy range to evaluate here, it's all calculated relative to the edge
 range_eval = np.concatenate((
-    np.arange(-20, 0, 3), np.arange(0, 100, 0.5), np.arange(100, 400, 2)
+    np.arange(-20, 0, 0.5), np.arange(0, 100, 1), np.arange(100, 400, 2)
 ))
 print(range_eval)
 
@@ -49,8 +50,8 @@ absorption = full_array[0:-1, 1]
 
 interp_abs = np.interp(range_eval + edge, energy, absorption)
 
-#plt.plot(energy, absorption)
-#plt.show()
+plt.plot(energy, absorption)
+plt.show()
 
-plt.plot(range_eval, interp_abs)
+plt.plot(range_eval, interp_abs, '--bo', label='Interpolated ' + element)
 plt.show()
